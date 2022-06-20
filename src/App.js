beb82a5154d0e02e7be7c51dfe209b95066837ae
jayson911 <jayson406@gmail.com>
2022-06-21 00:21:54 +0300

    hw1

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ function App() {
         name={'Homer Simpson'}
         link = {'https://simpsons.fandom.com/wiki/Homer_Simpson'}
         desc={'Homer Jay Simpson (born May 12, 1951)[36] is the main protagonist of The Simpsons series (or show). He is the spouse of Marge Simpson and father of Bart, Lisa and Maggie Simpson. Homer is overweight (said to be ~240 pounds), lazy, and often ignorant to the world around him. Although Homer has many flaws, he has shown to have great caring, love, and even bravery to those he cares about and, sometimes, even others he doesn\'t. He also serves as the main protagonist of the The Simpsons Movie. He is 40 years old and was born in 1951.'}
-        phrases={"D'oh!"}
-        phrases={"Why you little...!"}
-        phrases={"Woo-hoo!"}
         picture={'https://static.wikia.nocookie.net/simpsons/images/b/bd/Homer_Simpson.png'}
     />
 
--- src/components/character.js
+++ src/components/character.js
@@ -1,13 +1,11 @@
 import './character.css';
 
 function Character(props) {
-    let {name, link, desc, phrases, picture} = props;
+    let {name, link, desc, picture} = props;
 
     return (<div>
         <h2 className={'target asd'}><a href={link}> {name}</a></h2>
         <p>{desc}</p>
-        <ul>favorite phrase</ul>
-        <li>{phrases}</li>
         <img src={picture} alt=""/>
     </div>)
 
