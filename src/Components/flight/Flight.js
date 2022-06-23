23f8d380c93a74e60e436630fd0f29ea29fc3256
jayson911 <jayson406@gmail.com>
2022-06-23 11:57:30 +0300

    hw2-2

--- src/Components/flight/Flight.js
+++ src/Components/flight/Flight.js
@@ -2,9 +2,9 @@ export default function Flight({item}) {
 
     return(
         <div>
-            {item.flight_number}
-            {item.mission_name}
-            {item.launch_year}
+            {item.flight_number}{' '}
+            {item.mission_name}{' '}
+            {item.launch_year}{' '}
             <img src={item.links.mission_patch_small} alt="link"/>
         </div>
     );
